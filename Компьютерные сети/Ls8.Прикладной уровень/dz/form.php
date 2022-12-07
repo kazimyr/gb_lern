09b612ee18ad42824894d90d034f6b11cbef0d50
Kazimyr <kazimyr@gmail.com>
2022-12-07 17:48:07 +0500

    add Компьютерные сети

---
+++ Компьютерные сети/Ls8.Прикладной уровень/dz/form.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form method="POST" action="post.php"> <!--указание метода GET-->
+Login: <input type="text" name="login">
+<br> E-mail: <input type="text" name="email">
+<br> <input type="submit" value="Отправить">
+</form>
+</body>
+</html>
---
+++ Компьютерные сети/Ls8.Прикладной уровень/dz/post.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<?php // С помощью суперглобального массива $_POST // выводим принятые значения:
+echo "<br/>login = ". $_POST ['login'];
+echo "<br/>email = ". $_POST ['email'];?>
+</body>
+</html>
